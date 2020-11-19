7c449f4565a9760f39f309830bef45b51052c520
SvenPaterson <74683453+SvenPaterson@users.noreply.github.com>
2020-11-19 15:45:51 -0500

    Update TP_Exercise 3.16.py

--- TP_Exercise 3.16.py
+++ TP_Exercise 3.16.py
@@ -1,5 +1,5 @@
 """
-v1.1 11-19-20
+v1.2 11-19-20
 My solution for the 3.16 working example in Chap. 3 of
 Think Python by Allen Downey
 
@@ -7,52 +7,51 @@ I have improved on the challenge so that you can build
 a grid of any size by adjusting the variables num_rows
 and num_cols.
 
+Code to build unit is now encapsulated for simplicity
+
 I will add a user entry box in the code as the next task
 
 """
 
-num_rows = 7
-num_cols = 4
+num_rows = 3
+num_cols = 3
 
-t_r = "+ - - - -" #single grid beam unit
-m_r = "|        " #single row post unit
+t_r = "+ - - - -" #single unit beam
+m_r = "|        " #single unit post
 
-#function prints a three column beam and appends a final '+'
+#function prints out 
+def unit_print(a,b):
+    for i in range(b):
+        print(a,end=' ')
+        
+
+#function prints a 'beam' row with num_cols*columns
 def beam_print(arg):
-    i = 0
-    while i < num_cols:
-        print(arg, end=' ')
-        i = i + 1
+    unit_print(arg,num_cols)
     print('+')
+#
 
-#function prints a three column post and appends a final '|'
+#function prints a 'post'row with num_cols*columns
 def post_print(arg):
-    j = 0
-    while j < num_cols:
-        print(arg, end=' ')
-        j = j + 1
+    unit_print(arg,num_cols)        
     print("|")
+#
 
 #construct the middle of a block
 def do_four(f,arg):
-    f(arg)
-    f(arg)
-    f(arg)
-    f(arg)
-
+    for i in range(4):
+        f(arg)
+    
 def print_unit():
     beam_print(t_r)
     do_four(post_print, m_r)
+#
 
-#builds a three row grid
+#builds a grid with num_rows*rows
 def build_grid():
-    k = 0
-    while k < num_rows:
-        print_unit()
-        k = k+1
+    for i in range(num_rows):
+        print_unit()        
     beam_print(t_r)
+#
 
 build_grid()
-
-
-    
